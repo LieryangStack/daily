898102da62675fc043df01207c2b61bfbb34b11c
LieryangStack <SXlisibo@gmail.com>
2023-11-05 22:11:50 +0800

    update

--- 20231105/serve.c
+++ 20231105/serve.c
@@ -15,12 +15,16 @@ int main()
     perror("ftok");
     return 1;
   }
+  /* 0644 表示所有者具有读和写权限，属组和其他人只有读权限 */
   int shmid= shmget(key,SIZE,IPC_CREAT|IPC_EXCL|0644);//通过key创建出共享内存
   if(shmid<0)
   {
     perror("shmget");
     return 2;
   }
+  /**
+   * @param __shmflg：如果是0 就是读写模式，SHM_RD0NLY就是只读模式
+  */
   char* mem=(char*)shmat(shmid,NULL,0);//挂接共享内存
 
   //开始通信
